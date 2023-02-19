138410f32851fa8bab9a8231d8f443475c410c4d
cillvy5245 <bongaalhamdutaimako@gmail.com>
2023-02-19 04:53:03 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Entry pont
+  *
+  *Return: Always is 0 (success)
+  */
+
+int main(void)
+{
+	char joy;
+	int me;
+	long int we;
+	long long int be;
+	float cool;
+
+	prinf("%lu\n", sizeof(joy));
+	printf("&lu\n", sizeof(me));
+	printf("&lu\n", sizeof(we));
+	printf("&lu\n", sizeof(cool));
+
+	return (0);
+}
+
