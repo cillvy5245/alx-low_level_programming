9cade198092ff0aa11923a8cc7f49cc660eea701
cillvy5245 <bongaalhamdutaimako@gmail.com>
2023-02-20 04:27:32 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order,
+ * separated by a comma followed by a space.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int cillvy1, cillvy2, cillvy3;
+
+	for (cillvy1 = 0; cillvy1 < 8; cillvy++)
+	{
+		for (cillvy2 = cillvy1 + 1; cillvy2 < 9; cillvy++)
+		{
+			for (cillvy3 = cillvy2 + 1; cillvy3 < 10; cillvy++)
+			{
+				putchar((cillvy1 % 10) + '0');
+				putchar((cillvy2 % 10) + '0');
+				putchar((cillvy3 % 10) + '0');
+
+				if (cillvy1 == 7 && cillvy2 == 8 && cillvy3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
