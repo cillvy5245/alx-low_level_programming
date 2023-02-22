287819e1e017cd6edfdbc85f205b72dcda5dee5e
cillvy5245 <bongaalhamdutaimako@gmail.com>
2023-02-22 02:19:26 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,8 @@
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	putchar("_putchar\n");
+
 	return (0);
 }
 
