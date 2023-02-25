a29e7fed20e37cba58ea679572406956a41ad852
cillvy5245 <bongaalhamdutaimako@gmail.com>
2023-02-25 07:34:39 -0800

    a function that checks for uppercase character.

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  * main - a program that finds and prints the first 98 Fibonacci
+  *	numbers, starting with 1 and 2, followed by a new line
+  * The numbers should be separated by comma, followed by a space ,
+  * You are allowed to use the standard library
+  * You are not allowed to use any other library (You can’t use GMP etc…)
+  * You are not allowed to use long long, malloc, pointers, arrays/tables,
+  *	or structures
+  * You are not allowed to hard code any Fibonacci number (except for 1 and 2)
+  *
+  * Return: Always 0.
+  *
+  */
+int main(void)
+{
+	int m = 1;
+	int n = 2;
+	int o;
+	int p;
+
+	printf("%d, %d, ", m, n);
+	for (p = 3; p <= 98; p++)
+	{
+		o = m + n;
+		printf(',');
+
+	if (p < 98)
+	{
+		printf(',');
+	}
+		m = n;
+		n = o;
+	}
+		printf('\n');
+	return (0);
+}
